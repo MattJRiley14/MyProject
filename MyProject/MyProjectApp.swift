d10c9a30d65309ebc533550148e9428019deb78d
Matt Riley <matthew.riley@barren.kyschools.us>
2024-02-06 09:00:50 -0600

    Initial Commit

---
+++ MyProject/MyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectApp.swift
+//  MyProject
+//
+//  Created by BCCS 2022 on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
